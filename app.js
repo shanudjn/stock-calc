e57375c8a43955326e6d68ff4fa80c462bb0f451
Md Shahazad Uddin <shanudjn@gmail.com>
2021-01-16 22:35:06 +0530

    layout : added the basic html and css

---
+++ app.js
@@ -0,0 +1,12 @@
+// var apiKey = "YFP9V17BQF706RYS";
+var stockList = document.querySelector('#stocks-list');
+var price = document.querySelector('#price');
+var quantity = document.querySelector('#quantity');
+var checkBtn = document.querySelector('#check');
+
+var upEmoji = "🔼";
+var downEmoji = "🔽";
+
+
+
+
